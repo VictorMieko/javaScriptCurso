d1e504d4356ecf1b83ac5b60f24a44e336409cb0
VictorMieko <Victormiekooliveira@gmail.com>
2025-06-11 15:01:16 -0400

    Tentando inserir mais de uma opção de sorteio

--- teste/teste.js
+++ teste/teste.js
@@ -2,15 +2,24 @@ function escopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
-    function escolha(evento) {
-        evento.preventDefault();
+    function escolha(e) {
+        e.preventDefault();
 
-        const mensagem = form.querySelector('#mensagem').value.trim();
+        const mensagem = form.querySelector('.mensagem');
+        const opcoes = [];
+
+        mensagem.forEach(opcao => {
+            const texto = opcao.value.trim();
+            if(texto){
+                opcoes.push(texto);
+            }
+        });
         
-        if(mensagem.length === 0){
+        if(opcoes.length === 0){
             resultado.innerHTML = `<p>Insira pelo menos uma opção.</p>`;
+            return;
         }else{
-            const decisao = mensagem[Math.floor(Math.random() * mensagem.length)];
+            const decisao = mensagem[Math.floor(Math.random() * opcoes.length)];
             resultado.innerHTML = `<p>Palavra escolhida: ${decisao}</p>`;
         }
     }
