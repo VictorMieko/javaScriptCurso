31a8817b99a36520684c015810d6c1da917cc143
VictorMieko <Victormiekooliveira@gmail.com>
2025-06-27 19:00:18 -0400

    Calculadora com função fabricada

---
+++ FunçõesAvançados/aula75Calculadora/assets/js/main.js
@@ -0,0 +1,71 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+        inicia() {
+            this.cliqueButoes();
+            this.pressionaEnter();
+        },
+
+        cliqueButoes() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+                if (el.classList.contains('btn-del')) {
+                    this.deleteUm();
+                }
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+                this.display.focus();
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        deleteUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if (!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch (e) {
+                alert('Conta inválida');
+                return;
+            }
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', (e) => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            });
+        }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
