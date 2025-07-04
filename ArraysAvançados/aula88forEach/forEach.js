074d316d1b0e203df7b11d39aaeb1d51ec74e11c
VictorMieko <Victormiekooliveira@gmail.com>
2025-07-04 15:49:34 -0400

    Método forEach

---
+++ ArraysAvançados/aula88forEach/forEach.js
@@ -0,0 +1,9 @@
+//forEach só funciona para arrays
+//forEach faz interação de arrays
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let total = 0;
+a1.forEach(value => {
+        total += value;
+});
+console.log(total);
