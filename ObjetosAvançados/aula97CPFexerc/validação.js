6c3887c3630d3234c5b2b6ab275b7945e7feb588
VictorMieko <Victormiekooliveira@gmail.com>
2025-07-24 17:08:12 -0400

    Validação e criação de CPF aleatório

--- ObjetosAvançados/aula97CPFexerc/validação.js
+++ ObjetosAvançados/aula97CPFexerc/validação.js
@@ -14,10 +14,67 @@
  * 11 - (284 % 11) = 2 (Segundo dígito)
  */
 
-let cpf = '705.484.450-52'; // cpf deve ser tratodo como string
-let cpfLimpo = cpf.replace(/\D+/g, ''); // tudo que não for um número será substituido por nada
-console.log(cpfLimpo);
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        get: function () {
+            return cpfEnviado.replace(/\D+/g, ''); // tudo que não for um número será substituido por nada
+        }
+    });
+}
+ValidaCPF.prototype.valida = function () {
+    if (typeof this.cpfLimpo === "undefined") return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
 
-let cpfArray = Array.from(cpfLimpo);
-cpfArray = Array.map(el => el);
-cpfArray.reduce ((acumulador, value) => acumulador + Number(value), 0);
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+
+    return novoCpf === this.cpfLimpo;
+};
+ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let contador = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (contador * Number(val));
+        contador--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito); // garante que retorne como string
+};
+ValidaCPF.prototype.isSequencia = function () {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length); // repete o primeiro caracter do array e repete o tamanho dele(11 vezes)
+    return sequencia === this.cpfLimpo;
+};
+
+// Função que cria CPF completo aleatório
+function criaCpf() {
+    const cpfNovo = []; // cria um array para o cpf
+    while (cpfNovo.length < 9) { // enquanto o tamanho do array do cpf for menor que nove
+        let numero = Math.round(Math.random() * 9); // guarde o número aleatório na variável
+        cpfNovo.push(numero); // e coloque no array
+    }
+    const cpfParcial = cpfNovo.join(''); // transforma o CPF em STRING
+    const cpfTemp = new ValidaCPF(cpfParcial); // cria uma variável temporaria usando o CPF para utilizar os prototype do Objeto
+
+    const digito1 = cpfTemp.criaDigito(cpfParcial); // guarda o resultado do método do prototype aqui
+    const digito2 = cpfTemp.criaDigito(cpfParcial + digito1);
+
+    return cpfParcial + digito1 + digito2; // retorna o CPF aleatório com os dois ultimos dígitos corretos
+};
+
+const cpf = new ValidaCPF('705.484.450-52'); // cpf deve ser tratado como string
+let cpfValido = cpf.valida() === false ? `CPF 1: INVÁLIDO` : `CPF 1: VÁLIDO`;
+console.log(cpfValido);
+
+const cpfAleatorio = criaCpf();
+console.log(cpfAleatorio);
+
+const cpf2 = new ValidaCPF(cpfAleatorio);
+cpfValido = cpf2.valida() === false ? `CPF 2: inválido` : `CPF 2: válido`;
+console.log(cpfValido);
