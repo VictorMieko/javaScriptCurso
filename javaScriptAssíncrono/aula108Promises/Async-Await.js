337b2878de010f8fd54b0d15bca3d5240b97992e
VictorMieko <Victormiekooliveira@gmail.com>
2025-08-13 14:54:13 -0400

    Função Async e await em promises

--- javaScriptAssíncrono/aula108Promises/Async-Await.js
+++ javaScriptAssíncrono/aula108Promises/Async-Await.js
@@ -1,6 +1,6 @@
 // Usados para limpar mais as promises
 
-function rand(min=0, max=3) {
+function rand(min = 0, max = 3) {
     min *= 1000;
     max *= 1000;
     return Math.floor(Math.random() * (max - min) + min);
@@ -20,6 +20,23 @@ function espera(msg, tempo) {
     });
 }
 
+async function executa() {
+    try {
+        const fase1 = await espera('fase 1', rand()); // espera uma promise ser finzalizada
+        console.log(fase1 + ' async');
+
+        const fase2 = await espera('fase 2', rand());
+        console.log(fase2 + ' async');
+
+        const fase3 = await espera('fase 3', rand());
+        console.log('terminamos na fase 3:', fase3 + ' async')
+    } catch (e) {
+        console.log(e);
+    }
+}
+executa();
+
+/*
 espera('fase 1', rand())
     .then(value => {
         console.log(value);
@@ -27,5 +44,10 @@ espera('fase 1', rand())
     })
     .then(fase => {
         console.log(fase);
-        return espera('fase', rand());
-    });
+        return espera('fase 3', rand());
+    })
+    .then(fase => {
+        console.log(fase);
+    })
+    .catch(e => console.log(e));
+    */
